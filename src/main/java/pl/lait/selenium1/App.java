3d5ecad90a160d031e08e4f6f1a1c13b6d82f9bc
GryMich <GryMich@gmail.com>
2018-11-24 14:22:19 +0100

    Dodano pierwszy test

--- src/main/java/pl/lait/selenium1/App.java
+++ src/main/java/pl/lait/selenium1/App.java
@@ -9,20 +9,10 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  *
  */
 public class App 
-{
-	static WebDriver driver; 
-	
+{	
     public static void main( String[] args )
     {
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\MICHAŁ\\chromedriver.exe");
-    	DesiredCapabilities cap = DesiredCapabilities.chrome(); // DesiredCapabilities oświadcza na jakiej przeglądarce będzie działał test
-    	driver = new ChromeDriver(); 
-    	driver.get("http://newtours.demoaut.com");
-    	String pageTitle = driver.getTitle();
-    	System.out.println(pageTitle);
-    	driver.findElement(By.linkText("SIGN-ON")).click(); // tutaj program znajduje link po nazawie, a następnie na niego klika
-    	driver.findElement(By.name("userName")).sendKeys("Bobek");
-    	driver.findElement(By.name("password")).sendKeys("bobek");
-    	driver.findElement(By.name("login")).click();
-    	}
+    	
+    
+    }
 }
